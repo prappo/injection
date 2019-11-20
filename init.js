7d19a32602fc60b441ba7bd464969ce586051ba4
Prappo Prince <prappo.prince@gmail.com>
2019-11-20 21:45:31 +0600

    yoo

---
+++ init.js
@@ -0,0 +1,5 @@
+document.querySelector('head').innerHTML += '<link rel="stylesheet" href="http://localhost/test/injection/style.css" type="text/css"/>';
+extraNode = '<div id="myAd"> Your ads code here </div>';
+document.querySelector('body').innerHTML += extraNode;
+
+
